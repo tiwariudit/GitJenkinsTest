ab54567ef7121a001bf1eb61f2e2c19a62b2d63b
utiwari <udit.tiwari@here.com>
2017-06-22 17:35:30 +0530

    Initial commit

---
+++ gitMavenTest/src/main/java/com/gitmaventest/gitMavenTest/App.java
@@ -0,0 +1,13 @@
+package com.gitmaventest.gitMavenTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This Program is running from Git Jenkins." );
+    }
+}
