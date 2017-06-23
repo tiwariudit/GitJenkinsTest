ff3352b2c5616f70cf5de3289061dade215bbe57
utiwari <udit.tiwari@here.com>
2017-06-23 20:25:41 +0530

    Added a new project

---
+++ Demo/src/main/java/com/test/Demo/App.java
@@ -0,0 +1,13 @@
+package com.test.Demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is running from Jenkins" );
+    }
+}
